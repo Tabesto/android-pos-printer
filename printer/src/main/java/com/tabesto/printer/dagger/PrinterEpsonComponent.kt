a9cf54e4157cefb76ab5a97efe72d0e7ed0e662c
Raj <vigneshraj.radjasegar@tabesto.com>
2021-05-03 17:18:04 +0200

    feat: Inject and cancel coroutine scope inside Printer Epson (#39)
    * feat(job): inject coroutine scope inside printer epson
    
    * refactor(di): improve dagger implem
    
    * refactor(di): pass epos printer into parameter to  inject it
    
    Co-authored-by: Julien Herrero <julienherrero@tabesto.com>

--- printer/src/main/java/com/tabesto/printer/dagger/PrinterEpsonComponent.kt
+++ printer/src/main/java/com/tabesto/printer/dagger/PrinterEpsonComponent.kt
@@ -2,7 +2,9 @@ package com.tabesto.printer.dagger
 
 import com.tabesto.printer.PrinterEpson
 import dagger.Component
+import javax.inject.Singleton
 
+@Singleton
 @Component(modules = [PrinterEpsonModule::class])
 interface PrinterEpsonComponent {
     fun inject(printerEpson: PrinterEpson)
--- printer/src/main/java/com/tabesto/printer/dagger/PrinterEpsonModule.kt
+++ printer/src/main/java/com/tabesto/printer/dagger/PrinterEpsonModule.kt
@@ -2,31 +2,40 @@ package com.tabesto.printer.dagger
 
 import android.content.Context
 import com.tabesto.printer.model.PrinterData
+import com.tabesto.printer.model.PrinterModel
+import com.tabesto.printer.model.PrinterRegion
 import com.tabesto.printer.utils.EposPrinter
 import com.tabesto.printer.writer.PrinterWriter
 import com.tabesto.printer.writer.PrinterWriterListener
 import dagger.Module
 import dagger.Provides
-import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import javax.inject.Singleton
 
 @Module
-class PrinterEpsonModule(var printerData: PrinterData, var printerWriterListener: PrinterWriterListener, var context: Context) {
-    private lateinit var eposPrinter: EposPrinter
+
+class PrinterEpsonModule(
+    private val eposPrinter: EposPrinter? = null,
+    private val printerData: PrinterData? = null,
+    private val printerWriterListener: PrinterWriterListener,
+    private val context: Context
+) {
 
     @Provides
-    fun providesPrinterEpson(): EposPrinter {
-        eposPrinter = EposPrinter(printerData.printerModel.eposModel, printerData.printerRegion.eposRegion, context)
-        return eposPrinter
-    }
+    @Singleton
+    fun providesPrinterEpson(): EposPrinter = eposPrinter
+        ?: if (printerData != null) {
+            EposPrinter(printerData.printerModel.eposModel, printerData.printerRegion.eposRegion, context)
+        } else {
+            EposPrinter(PrinterModel.PRINTER_TM_M10.eposModel, PrinterRegion.PRINTER_ANK.eposRegion, context)
+        }
 
     @Provides
-    fun providesPrinterWriter(): PrinterWriter {
-        return PrinterWriter(eposPrinter, printerWriterListener)
-    }
+    @Singleton
+    fun providesPrinterWriter(eposPrinter: EposPrinter): PrinterWriter = PrinterWriter(eposPrinter, printerWriterListener)
 
     @Provides
-    fun providesDispatcher(): CoroutineDispatcher {
-        return Dispatchers.IO
-    }
+    @Singleton
+    fun providesCoroutineScope(): CoroutineScope = CoroutineScope(Dispatchers.IO)
 }
